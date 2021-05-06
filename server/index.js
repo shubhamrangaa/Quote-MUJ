27ae66ff1ef44a30ec245dc83090b2f4b0bc5827
boidushya <boidushyabhattacharya@gmail.com>
2021-05-06 13:20:46 +0530

    init setup

---
+++ server/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const blog = require("./blog");
+
+const app = express();
+
+// Middlewares
+
+app.set("trust proxy", 1);
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors({
+	origin: "*",
+}));
+
+app.use(express.json());
+app.use("/api",blog);
+
+// Base Route
+
+app.get("/",(req,res)=>{
+	res.json({
+		message: "🔐",
+		user: req.user,
+	});
+});
+
+// Error Handling
+
+const notFound = (req,res,next) => {
+	res.status(404);
+	const error = new Error(`Not Found = ${req.originalUrl}`);
+	next(error);
+}
+
+const errorHandler = (err,req,res,next) => {
+	res.status(res.statusCode || 500);
+	res.json({
+		message: err.message,
+		stack: err.stack,
+	});
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+// Listener and port
+
+const port = process.env.PORT || 5000;
+
+app.listen(port,()=>{
+	console.log(`Listening on port ${port}`);
+})
