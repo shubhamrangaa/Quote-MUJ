e12c36f5f705ed9758e302958e2af2a2f6cad4e0
Moonlightsculptor88 <mahipreetham@gmail.com>
2021-05-09 08:50:23 +0530

    Editing Footer

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -4,26 +4,29 @@ import { faInstagramSquare } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 function Footer() {
   return (
     <div className={styles.footer}>
       <div className={styles.left}>
+      <h1 className={styles.footerHeading}>The Quote Muj</h1>
         <div>
+        
           <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Podcasts</li>
-            <li>Vlog</li>
-            <li>Browse</li>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/">About</Link></li>
+            <li><Link href="/">Podcasts</Link></li>
+            <li><Link href="/">Vlogs</Link></li>
+            <li><Link href="/">Browse</Link></li>
           </ul>
         </div>
 
         <div>
           <ul>
-            <li>Categories</li>
-            <li>About</li>
-            <li>Browse</li>
+            <li><Link href="/">Categories</Link></li>
+            <li><Link href="/">About</Link></li>
+            <li><Link href="/">Browse</Link></li>
           </ul>
         </div>
       </div>
