a86bd49a8750a356b62b1b7fbd9372919a33f35c
Shubham Ranga <51476691+shubhamrangaa@users.noreply.github.com>
2021-05-10 19:55:15 +0530

    optimized for heroku

--- server/db.js
+++ server/db.js
@@ -1,11 +1,19 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
 
-const pool = new Pool({
-  user: "postgres",
-  password: "shubhama",
-  host: "localhost",
-  port: "5432",
-  database: "blogtrial",
-});
+const devConfig = {
+  user: process.env.POST_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+};
+
+const prodConfig = {
+  connectionString: process.env.DATABASE_URL,
+};
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? prodConfig : devConfig
+);
 
 module.exports = pool;
