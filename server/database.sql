b8289c5df8f60add3870936365caec02e83d6c07
Shubham Ranga <51476691+shubhamrangaa@users.noreply.github.com>
2021-05-10 20:32:16 +0530

    changes in database

--- server/database.sql
+++ server/database.sql
@@ -1,13 +1,13 @@
-CREATE DATABASE blog-trial;
+-- CREATE DATABASE blog-trial;
 
 CREATE TABLE blog(
     slug varchar(255) UNIQUE NOT NULL PRIMARY KEY,
     categories varchar(255) NOT NULL, 
     author varchar(255) NOT NULL, 
     heading text NOT NULL, 
-    caption text NOT NULL,q
-    article_data JSON NOT NULL,
-    date_created timestamptz  [0]  , 
+    caption text NOT NULL,
+    article_data json NOT NULL,
+    date_created timestamptz  [0], 
     likes int DEFAULT 0
 );
 
