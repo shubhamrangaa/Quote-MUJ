04b2d70065f9269bd42b7993d41f4c86c5fdc9c4
Kevin Peter <ultimatekevin1@gmail.com>
2021-05-25 23:33:12 +0530

    Fetch articles from backend

--- client/components/Story.js
+++ client/components/Story.js
@@ -1,33 +1,34 @@
 import mainstyles from "../styles/AllStories.module.scss";
-import Image from 'next/image';
-import { AiOutlineLike } from 'react-icons/ai';
+import Image from "next/image";
+import { AiOutlineLike } from "react-icons/ai";
 
-export default function Story (props) {
-    return (
-        <div className={mainstyles.story}>
-            <div>
-                <h3>This is the headline {props.heading}</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam posuere purus nec metus ullamcorper hendrerit. Aenean pulvinar convallis cursus. Ut nec tincidunt dui, varius dapibus magna. Nullam congue blandit ligula, eget rutrum augue dignissim sit amet.  {props.description}</p>
-                <div className={mainstyles.likesection} >
-                    <ul >
-                    <li>Random User</li>
-                    <li>Category</li>
-                    <li>15th June 2021</li>
-                    <li><AiOutlineLike className={mainstyles.likeicon} size="1.5em" /></li>
-                    <li>5 Likes</li>
-                    </ul>
-                </div>
-
-            </div>
-            <div>
-                <Image className={mainstyles.image}
-                    src="/img.png"
-                    alt="Picture of the author"
-                    width={300}
-                    height={300}
-                />
-            </div>
-
+export default function Story(props) {
+  return (
+    <div className={mainstyles.story}>
+      <div>
+        <h3>{props.heading}</h3>
+        <p>{props.caption}</p>
+        <div className={mainstyles.likesection}>
+          <ul>
+            <li>Author: {props.author}</li>
+            <li>Category: {props.categories}</li>
+            <li>{props.date_created}</li>
+            <li>
+              <AiOutlineLike className={mainstyles.likeicon} size="1.5em" />
+            </li>
+            <li>{props.likes} Likes</li>
+          </ul>
         </div>
-    )
+      </div>
+      <div>
+        <Image
+          className={mainstyles.image}
+          src="/img.png"
+          alt="Picture of the author"
+          width={200}
+          height={200}
+        />
+      </div>
+    </div>
+  );
 }
