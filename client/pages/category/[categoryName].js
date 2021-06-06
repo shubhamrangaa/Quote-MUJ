80f48506319c20b9008a6f774ca35dc9d715a1e1
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2021-06-06 22:08:47 +0530

    added Trending Carousel

--- client/pages/category/[categoryName].js
+++ client/pages/category/[categoryName].js
@@ -1,25 +1,27 @@
 import React from "react";
 import { useRouter } from "next/router";
 import styles from "../../styles/MonthlyNewsletter.module.scss";
+import TrendingCarousel from "../../components/TrendingCarousel";
 
 const Category = () => {
-  const router = useRouter();
-  const category = router.query.categoryName;
-  return (
-    <>
-      <section className={styles.top}>
-        <div className={styles.headingcontainer}>
-          <h1 className={styles.heading}>{category}</h1>
-        </div>
-        <div className={styles.rect}>
-          <div className={styles.rect__one}></div>
-          <div className={styles.rect__two}></div>
-          <div className={styles.rect__three}></div>
-          <div className={styles.rect__four}></div>
-        </div>
-      </section>
-    </>
-  );
+	const router = useRouter();
+	const category = router.query.categoryName;
+	return (
+		<>
+			<section className={styles.top}>
+				<div className={styles.headingcontainer}>
+					<h1 className={styles.heading}>{category}</h1>
+				</div>
+				<div className={styles.rect}>
+					<div className={styles.rect__one}></div>
+					<div className={styles.rect__two}></div>
+					<div className={styles.rect__three}></div>
+					<div className={styles.rect__four}></div>
+				</div>
+			</section>
+			<TrendingCarousel />
+		</>
+	);
 };
 
 export default Category;
