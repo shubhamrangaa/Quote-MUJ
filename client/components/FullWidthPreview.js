8a382bad4e1aa10048e89ad5e63efbc605e55f4a
manavendrasen <manavendra.199302058@muj.manipal.edu>
2021-07-02 16:32:25 +0530

    fix: linked FullWidthPreview

--- client/components/FullWidthPreview.js
+++ client/components/FullWidthPreview.js
@@ -1,18 +1,24 @@
 import React from "react";
 import styles from "../styles/FullWidthPreview.module.scss";
+import Link from "next/link";
 
 const FullWidthPreview = (props) => {
-	return (
-		<div className={styles.container}>
-			<div className={styles.textContainer}>
-				<div className={styles.textHeading}>{props.heading}</div>
-				<div className={styles.textBody}>{props.body}</div>
-				<div className={styles.textType}>{props.type}</div>
-			</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <Link href={`/blogs/${props.slug}`}>
+          <div className={styles.textHeading}>{props.heading}</div>
+        </Link>
+        <div className={styles.textBody}>{props.body}</div>
+        <div className={styles.textType}>{props.type}</div>
+      </div>
 
-			<img className={styles.imageContainer} src={props.image ? props.image : "https://picsum.photos/300/200"} />
-		</div>
-	);
+      <img
+        className={styles.imageContainer}
+        src={props.image ? props.image : "https://picsum.photos/300/200"}
+      />
+    </div>
+  );
 };
 
 export default FullWidthPreview;
