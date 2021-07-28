d2417fd22213efd6ba3c443d790083e2b843f7d9
manavendrasen <manavendra4288@gmail.com>
2021-07-28 15:06:00 +0530

    fix: add gkp

--- client/components/home/Collage.js
+++ client/components/home/Collage.js
@@ -5,22 +5,22 @@ export const Collage = () => {
   return (
     <div className={styles.collage}>
       <div id={styles.img1}>
-        <img src="/assets/Mn-3P_jMAbU.png" alt="img1"></img>
+        <img src="/assets/Mn-3P_jMAbU.png" height={145} alt="img1"></img>
       </div>
       <div id={styles.img2}>
         <img src="/assets/Bz3aP1lagp4.png" alt="img2"></img>
       </div>
       <div id={styles.img3}>
-        <img src="/assets/image 3.png" alt="img3"></img>
+        <img src="/assets/image 3.png" width={150} alt="img3"></img>
       </div>
       <div id={styles.img4}>
-        <img src="/assets/image 4.png" alt="img4"></img>
+        <img src="/assets/image 4.png" height={200} alt="img4"></img>
       </div>
       <div id={styles.img5}>
-        <img src="/assets/UfgeNK-KCuU.png" alt="img5"></img>
+        <img src="/assets/image 4.jpg" width={175} alt="img5"></img>
       </div>
       <div id={styles.img6}>
-        <img id={styles.img6} src="/assets/Rectangle.png" alt="img6"></img>
+        <img id={styles.img6} src="/assets/Rectangle.png" height={150} alt="img6"></img>
       </div>
     </div>
   );
