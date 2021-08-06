dcc554f6de5707bb00e501ae8d285ab18a71ba94
manavendrasen <manavendra4288@gmail.com>
2021-08-06 10:53:51 +0530

    feat: leadership message cards

---
+++ client/components/home/LeadershipCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+
+import styles from "@styles/LeadershipMessage.module.scss";
+
+const LeadershipCard = ({ href, image, title, children }) => {
+  return (
+    <Link href={`/message/${href}`}>
+      <div className={styles.card}>
+        <div className={styles.cardImage}>
+          <img src={image} alt='title' />
+        </div>
+        <h3>{title}</h3>
+        <p>{children}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default LeadershipCard;
--- client/components/home/LeadershipMessage.js
+++ client/components/home/LeadershipMessage.js
@@ -1,27 +1,51 @@
 import React from "react";
+import LeadershipCard from "./LeadershipCard";
 import styles from "@styles/LeadershipMessage.module.scss";
 import { sectionHeading, decorated } from "@styles/Heading.module.scss";
 import Link from "next/link";
 
 const LeadershipMessage = () => {
-	return (
-		<div className={styles.container}>
-			<h3 className={decorated + " " + sectionHeading}>
-				<span>Leadership Message</span>
-			</h3>
-			<div className={styles.tray}>
-				<Link href='/message/president'>
-					<div className={styles.secondary}>President's Message</div>
-				</Link>
-				<Link href='/message/pro_president'>
-					<div className={styles.secondary}>Pro President's Message</div>
-				</Link>
-				<Link href='/message/registrar'>
-					<div className={styles.secondary}>Registrar's Message</div>
-				</Link>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      <h3 className={decorated + " " + sectionHeading}>
+        <span>Leadership Message</span>
+      </h3>
+      <div className={styles.tray}>
+        <LeadershipCard
+          href='president'
+          image='/assets/og_gkprabhu.jpg'
+          title="President's Message"
+        >
+          Manipal University Jaipur, MUJ, as it is fondly called has completed
+          ten golden years of its establishment. This journey of ten long years
+          has been very enriching...
+        </LeadershipCard>
+        <LeadershipCard
+          href='pro_president'
+          image='/assets/nnsharma.jpg'
+          title="Pro President's Message"
+        >
+          Manipal University Jaipur is envisaged to be an enduring global center
+          of excellence in higher education, and is driven by erudite team...
+        </LeadershipCard>
+        <LeadershipCard
+          href='registrar'
+          image='/assets/nbhatnagar.jpg'
+          title="Registrar's Message"
+        >
+          The Manipal Group has extended its legacy in terms of focused Vision,
+          Mission and Values in education, research, and healthcare...
+        </LeadershipCard>
+        {/* 
+        <Link href='/message/pro_president'>
+          <div className={styles.secondary}>Pro President's Message</div>
+        </Link>
+        <Link href='/message/registrar'>
+          <div className={styles.secondary}>Registrar's Message</div>
+        </Link> */}
+      </div>
+    </div>
+  );
 };
 
 export default LeadershipMessage;
