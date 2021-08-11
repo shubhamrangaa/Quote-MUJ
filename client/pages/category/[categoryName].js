3ac0c66406ee809c8e8ac4b6b6f33dedd45f0a4e
shubham <shubhamranga@gmail.com>
2021-08-12 01:39:45 +0530

    fix: upcoming events

--- client/pages/category/[categoryName].js
+++ client/pages/category/[categoryName].js
@@ -10,77 +10,96 @@ import FullWidthPreview from "@components/FullWidthPreview";
 import TrendingCarousel from "@components/TrendingCarousel";
 
 export const getStaticPaths = async () => {
-	return {
-		paths: [],
-		fallback: "blocking",
-	};
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
 };
 
 export async function getStaticProps({ params }) {
-	let { categoryName } = params;
-	categoryName = categoryName.charAt(0).toUpperCase() + categoryName.slice(1);
-	//   console.log(categoryName);
+  let { categoryName } = params;
+  categoryName = categoryName.charAt(0).toUpperCase() + categoryName.slice(1);
+  //   console.log(categoryName);
 
-	let res = await fetch(`https://quote-muj.herokuapp.com/api/blogs/categories/${categoryName}`);
-	if(categoryName === "upcoming-events"){
-	 res = await fetch(`https://quote-muj.herokuapp.com/api/blogs/categories/Upcoming`);
-		console.log("hello");
-	}
-	const news = await res.json();
-	//   console.log(news);
-	return { props: { news } };
+  let res = await fetch(
+    `https://quote-muj.herokuapp.com/api/blogs/categories/${categoryName}`
+  );
+  console.log(categoryName);
+  if (categoryName === "Upcoming-events") {
+    res = await fetch(
+      `https://quote-muj.herokuapp.com/api/blogs/categories/Upcoming`
+    );
+    console.log("hello");
+  }
+  const news = await res.json();
+  //   console.log(news);
+  return { props: { news } };
 }
 
 const Category = ({ news }) => {
-	const { query } = useRouter();
-	let { categoryName } = query;
-	//   console.log(categoryName);
-	categoryName = categoryName.charAt(0).toUpperCase() + categoryName.slice(1);
-	news = news.map(item => {
-		try {
-			return {
-				...item,
-				images: JSON.parse(item.images)[0]
-			}
-		}
-		catch {
-			return {
-				...item,
-				images: item.images
-			}
-		}
-	})
-	const headlinerData = news.slice(0, 1)[0];
-	const headlinerAsideData = news.slice(1, 4);
-	const otherNewsData = news.slice(5);
-	return (
-		<>
-			{Object.keys(news).length ? (
-				<div>
-				{/* biggest off in history */}
-					<PageHeader heading={categoryName === 'Accreditation' ? 'Ranking & Accreditation' : categoryName} />
-					<section>
-						{/* todo: style heading */}
-						{/* <h2>Headliner</h2> */}
-						<div className={styles.headlinerContainer}>
-							<HeadlinerMain
-								headline={headlinerData.heading}
-								content={headlinerData.caption}
-								image={headlinerData.images}
-								tags={headlinerData.categories}
-								slug={headlinerData.slug}
-							/>
-							<div className={styles.headlinerAsideContainer}>
-								{/* map headlineAside */}
-								{headlinerAsideData.map((data, index) => {
-									return <HeadlinerAside key={index} headline={data.heading} description={data.caption.slice(0, 150) + "..."} image={data.images} author={data.author} slug={data.slug} />;
-								})}
-							</div>
-						</div>
-					</section>
-					<TrendingCarousel news={news} />
-					{/* Other News Section */}
-					{/* <section>
+  const { query } = useRouter();
+  let { categoryName } = query;
+  //   console.log(categoryName);
+  categoryName = categoryName.charAt(0).toUpperCase() + categoryName.slice(1);
+  news = news.map((item) => {
+    try {
+      return {
+        ...item,
+        images: JSON.parse(item.images)[0],
+      };
+    } catch {
+      return {
+        ...item,
+        images: item.images,
+      };
+    }
+  });
+  const headlinerData = news.slice(0, 1)[0];
+  const headlinerAsideData = news.slice(1, 4);
+  const otherNewsData = news.slice(5);
+  return (
+    <>
+      {Object.keys(news).length ? (
+        <div>
+          {/* biggest off in history */}
+          <PageHeader
+            heading={
+              categoryName === "Accreditation"
+                ? "Ranking & Accreditation"
+                : categoryName
+            }
+          />
+          <section>
+            {/* todo: style heading */}
+            {/* <h2>Headliner</h2> */}
+            <div className={styles.headlinerContainer}>
+              <HeadlinerMain
+                headline={headlinerData.heading}
+                content={headlinerData.caption}
+                image={headlinerData.images}
+                tags={headlinerData.categories}
+                slug={headlinerData.slug}
+              />
+              <div className={styles.headlinerAsideContainer}>
+                {/* map headlineAside */}
+                {headlinerAsideData.map((data, index) => {
+                  return (
+                    <HeadlinerAside
+                      key={index}
+                      headline={data.heading}
+                      description={data.caption.slice(0, 150) + "..."}
+                      image={data.images}
+                      author={data.author}
+                      slug={data.slug}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          </section>
+          <TrendingCarousel news={news} />
+          {/* Other News Section */}
+          {/* <section>
 						<div className={styles.subHeadingContainer}>
 							<div className={styles.subHeadingLine} />
 							<div className={styles.subHeadingText}>Other News</div>
@@ -93,20 +112,21 @@ const Category = ({ news }) => {
 						))}
 						<div className={styles.viewAllButton}>Explore all topics</div>
 					</section> */}
-				</div>
-			) : (
-				<div
-					style={{
-						height: "100vh",
-						display: "flex",
-						alignItems: "center",
-						justifyContent: "center",
-					}}>
-					No Articles
-				</div>
-			)}
-		</>
-	);
+        </div>
+      ) : (
+        <div
+          style={{
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          No Articles
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Category;
