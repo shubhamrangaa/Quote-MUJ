be1d535b531880ae4348628455190bf05bb8f91f
manavendrasen <manavendra4288@gmail.com>
2021-09-01 16:26:35 +0530

    feat: images change

--- client/components/home/Collage.js
+++ client/components/home/Collage.js
@@ -5,22 +5,22 @@ export const Collage = () => {
   return (
     <div className={styles.collage}>
       <div id={styles.img1}>
-        <img src="/assets/collage/1.jpg" height={200} alt="img1"></img>
+        <img src="/assets/collage/1.jpg" height={400} alt="img1"></img>
       </div>
       <div id={styles.img2}>
-        <img src="/assets/collage/2.jpg" height={200} alt="img2"></img>
+        <img src="/assets/collage/2.jpg" height={250} alt="img2"></img>
       </div>
       <div id={styles.img3}>
-        <img src="/assets/collage/3.jpg" height={200} alt="img3"></img>
+        <img src="/assets/collage/3.jpg" height={250} alt="img3"></img>
       </div>
       <div id={styles.img4}>
-        <img src="/assets/collage/4.jpg" height={200} alt="img4"></img>
+        <img src="/assets/collage/4.jpg" height={250} alt="img4"></img>
       </div>
-      <div id={styles.img5}>
+      {/* <div id={styles.img5}>
         <img src="/assets/collage/5.jpg" height={200} alt="img5"></img>
-      </div>
+      </div> */}
       <div id={styles.img6}>
-        <img id={styles.img6}src="/assets/collage/6.jpg" height={200} alt="img6"></img>
+        <img src="/assets/collage/6.jpg" height={250} alt="img6"></img>
       </div>
     </div>
   );
