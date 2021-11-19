2d845acc955d5a3573f6ed15949ae144f7a9a3d2
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2021-11-19 10:57:25 +0530

    fix: parse null images

--- client/pages/monthly/[year]/[month].js
+++ client/pages/monthly/[year]/[month].js
@@ -7,115 +7,106 @@ import HeadlinerAside from "@components/HeadlinerAside";
 import FullWidthPreview from "@components/FullWidthPreview";
 
 export async function getServerSideProps({ params }) {
-  const { month } = params;
-  const res = await fetch(
-    `https://quote-muj.herokuapp.com/api/blogs/monthly/${month}`
-  );
-  const news = await res.json();
-  return { props: { news } };
+	const { month } = params;
+	const res = await fetch(`https://quote-muj.herokuapp.com/api/blogs/monthly/${month}`);
+	const news = await res.json();
+	return { props: { news } };
 }
 
 const Newsletter = ({ news }) => {
-  const { query } = useRouter();
-  const { year, month } = query;
-  const headlinerData = news.slice(0, 3);
-  const headlinerAsideData = news.slice(3, 4);
-  const otherNewsData = news.slice(5);
-  return (
-    <>
-      {Object.keys(news).length ? (
-        <div>
-          <section className={styles.top}>
-            <div className={styles.headingcontainer}>
-              <h1 className={styles.heading}>Monthly Newsletter</h1>
-              <h4>Inaugural Issue</h4>
-            </div>
-          </section>
+	const { query } = useRouter();
+	const { year, month } = query;
+	const headlinerData = news.slice(0, 3);
+	const headlinerAsideData = news.slice(3, 4);
+	const otherNewsData = news.slice(5);
+	return (
+		<>
+			{Object.keys(news).length ? (
+				<div>
+					<section className={styles.top}>
+						<div className={styles.headingcontainer}>
+							<h1 className={styles.heading}>Monthly Newsletter</h1>
+							<h4>Inaugural Issue</h4>
+						</div>
+					</section>
 
-          {/* Headliner body */}
+					{/* Headliner body */}
 
-          <section>
-            {/* todo: style heading */}
-            {/* <h2 className={sectionHeading}>Headliners</h2> */}
-            <div className={styles.subHeadingContainer}>
-              <h3 className={decorated + " " + sectionHeading}>
-                <span>Headlines</span>
-              </h3>
-            </div>
-            <div className={styles.headlinerContainer}>
-              {headlinerData.map((data, id) => {
-                return (
-                  <HeadlinerAside
-                    key={id}
-                    headline={data.heading}
-                    description={data.caption.slice(0, 150) + "..."}
-                    image={JSON.parse(data.images)[0]}
-                    author={data.author}
-                    slug={data.slug}
-                  />
-                );
-              })}
-              <div className={styles.headlinerAsideContainer}>
-                {/* map headlineAside */}
-                {headlinerAsideData.map((data, id) => {
-                  return (
-                    <HeadlinerAside
-                      key={id}
-                      headline={data.heading}
-                      description={data.caption.slice(0, 150) + "..."}
-                      image={JSON.parse(data.images)[0]}
-                      author={data.author}
-                      slug={data.slug}
-                    />
-                  );
-                })}
-              </div>
-            </div>
-          </section>
+					<section>
+						{/* todo: style heading */}
+						{/* <h2 className={sectionHeading}>Headliners</h2> */}
+						<div className={styles.subHeadingContainer}>
+							<h3 className={decorated + " " + sectionHeading}>
+								<span>Headlines</span>
+							</h3>
+						</div>
+						<div className={styles.headlinerContainer}>
+							{headlinerData.map((data, id) => {
+								return (
+									<HeadlinerAside
+										key={id}
+										headline={data.heading}
+										description={data.caption.slice(0, 150) + "..."}
+										image={JSON.parse(data.images)[0]}
+										author={data.author}
+										slug={data.slug}
+									/>
+								);
+							})}
+							<div className={styles.headlinerAsideContainer}>
+								{/* map headlineAside */}
+								{headlinerAsideData.map((data, id) => {
+									return (
+										<HeadlinerAside
+											key={id}
+											headline={data.heading}
+											description={data.caption.slice(0, 150) + "..."}
+											image={data.images ? JSON.parse(data.images)[0] : null}
+											author={data.author}
+											slug={data.slug}
+										/>
+									);
+								})}
+							</div>
+						</div>
+					</section>
 
-          {/* More News Section */}
-          <section>
-            <div className={styles.subHeadingContainer}>
-              <h3 className={decorated + " " + sectionHeading}>
-                <span>More News</span>
-              </h3>
-            </div>
-            {otherNewsData.map((data, i) => {
-              let image = "";
-              try {
-                image = JSON.parse(data.images)[0];
-              } catch (e) {
-                image = data.images;
-              }
-              return (
-                <div key={i.toString()}>
-                  <FullWidthPreview
-                    slug={data.slug}
-                    heading={data.heading}
-                    body={data.caption}
-                    image={image}
-                    type={data.type}
-                  />
-                </div>
-              );
-            })}
-            <div className={styles.viewAllButton}>Explore all topics</div>
-          </section>
-        </div>
-      ) : (
-        <div
-          style={{
-            height: "100vh",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          No Articles
-        </div>
-      )}
-    </>
-  );
+					{/* More News Section */}
+					<section>
+						<div className={styles.subHeadingContainer}>
+							<h3 className={decorated + " " + sectionHeading}>
+								<span>More News</span>
+							</h3>
+						</div>
+						{otherNewsData.map((data, i) => {
+							let image = "";
+							try {
+								image = JSON.parse(data.images)[0];
+							} catch (e) {
+								image = data.images;
+							}
+							return (
+								<div key={i.toString()}>
+									<FullWidthPreview slug={data.slug} heading={data.heading} body={data.caption} image={image} type={data.type} />
+								</div>
+							);
+						})}
+						<div className={styles.viewAllButton}>Explore all topics</div>
+					</section>
+				</div>
+			) : (
+				<div
+					style={{
+						height: "100vh",
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "center",
+					}}>
+					No Articles
+				</div>
+			)}
+		</>
+	);
 };
 
 export default Newsletter;
