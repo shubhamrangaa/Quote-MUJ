e8f8fd4929d587a932d085f6b7102f72c1e8706e
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2021-11-19 11:14:17 +0530

    fix: default image dome

--- client/components/HeadlinerAside.js
+++ client/components/HeadlinerAside.js
@@ -7,28 +7,28 @@ import styles from "@styles/Headliner.module.scss";
 
 // HeadlinerAside acts like a card, use map()
 const HeadlinerAside = (props) => {
-  const { headline, image, description, author, slug } = props;
-  return (
-    <div className={styles.headlinerAsideContainer}>
-      <div className={styles.imageContainer}>
-        <img src={image ? image : "https://picsum.photos/300/200"} />
-      </div>
-      <div>
-        <Link href={`/blogs/${slug}`}>
-          <a>{headline}</a>
-        </Link>
-        <p className={styles.author}>By {author}</p>
-        <p className={styles.description}>{description}</p>
-      </div>
-    </div>
-  );
+	const { headline, image, description, author, slug } = props;
+	return (
+		<div className={styles.headlinerAsideContainer}>
+			<div className={styles.imageContainer}>
+				<img src={image ? image : "https://res.cloudinary.com/quote-muj/image/upload/q_40/v1637300211/manipal-1595339469_h298bf.jpg"} />
+			</div>
+			<div>
+				<Link href={`/blogs/${slug}`}>
+					<a>{headline}</a>
+				</Link>
+				<p className={styles.author}>By {author}</p>
+				<p className={styles.description}>{description}</p>
+			</div>
+		</div>
+	);
 };
 
 HeadlinerAside.prototype = {
-  headline: PropTypes.string.isRequired,
-  imageMetaData: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
+	headline: PropTypes.string.isRequired,
+	imageMetaData: PropTypes.string.isRequired,
+	author: PropTypes.string.isRequired,
+	description: PropTypes.string.isRequired,
 };
 
 export default HeadlinerAside;
--- client/pages/category/[categoryName].js
+++ client/pages/category/[categoryName].js
@@ -10,95 +10,85 @@ import FullWidthPreview from "@components/FullWidthPreview";
 import TrendingCarousel from "@components/TrendingCarousel";
 
 export async function getServerSideProps({ params }) {
-  let { categoryName } = params;
-  categoryName = categoryName.charAt(0).toUpperCase() + categoryName.slice(1);
+	let { categoryName } = params;
+	categoryName = categoryName.charAt(0).toUpperCase() + categoryName.slice(1);
 
-  let res = await fetch(
-    `https://quote-muj.herokuapp.com/api/blogs/categories/${categoryName}`
-  );
-  if (categoryName === "Upcoming-events") {
-    res = await fetch(
-      `https://quote-muj.herokuapp.com/api/blogs/categories/Upcoming`
-    );
-  }
-  const news = await res.json();
-  let sortedNews = news
-    .sort(function (a, b) {
-      let c = new Date(a.date_created);
-      let d = new Date(b.date_created);
-      return c - d;
-    })
-    .reverse();
-  return { props: { sortedNews } };
+	let res = await fetch(`https://quote-muj.herokuapp.com/api/blogs/categories/${categoryName}`);
+	if (categoryName === "Upcoming-events") {
+		res = await fetch(`https://quote-muj.herokuapp.com/api/blogs/categories/Upcoming`);
+	}
+	const news = await res.json();
+	let sortedNews = news
+		.sort(function (a, b) {
+			let c = new Date(a.date_created);
+			let d = new Date(b.date_created);
+			return c - d;
+		})
+		.reverse();
+	return { props: { sortedNews } };
 }
 
 const Category = ({ sortedNews }) => {
-  const { query } = useRouter();
-  let { categoryName } = query;
-  categoryName = categoryName.charAt(0).toUpperCase() + categoryName.slice(1);
-  sortedNews = sortedNews.map((item) => {
-    try {
-      return {
-        ...item,
-        images: JSON.parse(item.images)[0],
-      };
-    } catch {
-      return {
-        ...item,
-        images: item.images,
-      };
-    }
-  });
-  const headlinerData = sortedNews.slice(0, 3);
-  const headlinerAsideData = sortedNews.slice(3, 4);
-  return (
-    <>
-      {Object.keys(sortedNews).length ? (
-        <div>
-          {/* biggest off in history */}
-          <PageHeader
-            heading={
-              categoryName === "Accreditation"
-                ? "Ranking & \n Accreditation"
-                : categoryName
-            }
-          />
-          <section>
-            {/* todo: style heading */}
-            {/* <h2>Headliner</h2> */}
-            <div className={styles.headlinerContainer}>
-              {headlinerData.map((data, index) => {
-                return (
-                  <HeadlinerAside
-                    key={index}
-                    headline={data.heading}
-                    description={data.caption.slice(0, 150) + "..."}
-                    image={data.images}
-                    author={data.author}
-                    slug={data.slug}
-                  />
-                );
-              })}
-              <div className={styles.headlinerAsideContainer}>
-                {/* map headlineAside */}
-                {headlinerAsideData.map((data, index) => {
-                  return (
-                    <HeadlinerAside
-                      key={index}
-                      headline={data.heading}
-                      description={data.caption.slice(0, 150) + "..."}
-                      image={data.images}
-                      author={data.author}
-                      slug={data.slug}
-                    />
-                  );
-                })}
-              </div>
-            </div>
-          </section>
-          <TrendingCarousel news={sortedNews} />
-          {/* More News Section */}
-          {/* <section>
+	const { query } = useRouter();
+	let { categoryName } = query;
+	categoryName = categoryName.charAt(0).toUpperCase() + categoryName.slice(1);
+	sortedNews = sortedNews.map((item) => {
+		try {
+			return {
+				...item,
+				images: item.images ? JSON.parse(item.images)[0] : null,
+			};
+		} catch {
+			return {
+				...item,
+				images: item.images,
+			};
+		}
+	});
+	const headlinerData = sortedNews.slice(0, 30);
+	const headlinerAsideData = sortedNews.slice(3, 4);
+	return (
+		<>
+			{Object.keys(sortedNews).length ? (
+				<div>
+					{/* biggest off in history */}
+					<PageHeader heading={categoryName === "Accreditation" ? "Ranking & \n Accreditation" : categoryName} />
+					<section>
+						{/* todo: style heading */}
+						{/* <h2>Headliner</h2> */}
+						<div className={styles.headlinerContainer}>
+							{headlinerData.map((data, index) => {
+								return (
+									<HeadlinerAside
+										key={index}
+										headline={data.heading}
+										description={data.caption.slice(0, 150) + "..."}
+										image={data.images}
+										author={data.author}
+										slug={data.slug}
+									/>
+								);
+							})}
+							<div className={styles.headlinerAsideContainer}>
+								{/* map headlineAside */}
+								{headlinerAsideData.map((data, index) => {
+									return (
+										<HeadlinerAside
+											key={index}
+											headline={data.heading}
+											description={data.caption.slice(0, 150) + "..."}
+											image={data.images}
+											author={data.author}
+											slug={data.slug}
+										/>
+									);
+								})}
+							</div>
+						</div>
+					</section>
+					<TrendingCarousel news={sortedNews} />
+					{/* More News Section */}
+					{/* <section>
 						<div className={styles.subHeadingContainer}>
 							<div className={styles.subHeadingLine} />
 							<div className={styles.subHeadingText}>More News</div>
@@ -111,21 +101,20 @@ const Category = ({ sortedNews }) => {
 						))}
 						<div className={styles.viewAllButton}>Explore all topics</div>
 					</section> */}
-        </div>
-      ) : (
-        <div
-          style={{
-            height: "100vh",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          No Articles
-        </div>
-      )}
-    </>
-  );
+				</div>
+			) : (
+				<div
+					style={{
+						height: "100vh",
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "center",
+					}}>
+					No Articles
+				</div>
+			)}
+		</>
+	);
 };
 
 export default Category;
