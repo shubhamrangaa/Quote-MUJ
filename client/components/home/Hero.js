fc1cd7d00f23075ce84cfbc24b19299c642cc8c9
Manavendra Sen <manavendra4288@gmail.com>
2022-06-23 14:06:32 +0530

    feat: may issue link change

--- client/components/home/Hero.js
+++ client/components/home/Hero.js
@@ -64,7 +64,7 @@ export const Hero = () => {
             </div>
             <p id={styles.intro}>Welcome to the official E-Newsletter of</p>
             <h3 className={styles.heading}>Manipal University Jaipur</h3>
-            <Link href={`/monthly/2022/April`}>
+            <Link href={`/monthly/2022/May`}>
               <a id={styles.explore}>Explore May Issue</a>
             </Link>
           </div>
