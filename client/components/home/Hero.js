b1ae3c4a86e18e521a7e8a46b59cae2ba1a7e872
Manavendra Sen <manavendra4288@gmail.com>
2022-09-21 12:00:16 +0530

    feat: August issue

--- client/components/home/Hero.js
+++ client/components/home/Hero.js
@@ -64,8 +64,8 @@ export const Hero = () => {
             </div>
             <p id={styles.intro}>Welcome to the official E-Newsletter of</p>
             <h3 className={styles.heading}>Manipal University Jaipur</h3>
-            <Link href={`/monthly/2022/August`}>
-              <a id={styles.explore}>Explore July Issue</a>
+            <Link href={`/monthly/2022/July`}>
+              <a id={styles.explore}>Explore August Issue</a>
             </Link>
           </div>
         </div>
